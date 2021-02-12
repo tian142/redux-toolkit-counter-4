c17979981fe2847e80f7faa6102a85ba45fc3776
Tian <tian.qiu142@gmail.com>
2021-02-12 12:30:30 -0800

    completed adding selector and dispatch in feature file, completed project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
+import React, { useState } from "react"
 import "./App.css"
+import {
+  increment,
+  decrement,
+  incrementBy,
+  incrementAsync,
+  selectCounter,
+} from "./counterSlice"
+import { useSelector, useDispatch } from "react-redux"
 
 function App() {
+  const [inputValue, setInputValue] = useState(0)
+  const dispatch = useDispatch()
+  const value = useSelector(selectCounter)
+
   return (
     <div>
-      <h1>0</h1>
-      <button>+ 1</button>
-      <button>- 1</button>
+      <h1>{value}</h1>
+      <button onClick={() => dispatch(increment())}>+ 1</button>
+      <button onClick={() => dispatch(decrement())}>- 1</button>
 
       <div>
-        <input />
-        <button>Increment By</button>
-        <button>Decrement By</button>
+        <input
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={() => dispatch(incrementBy(Number(inputValue) || 0))}>
+          Increment By
+        </button>
+        <button
+          onClick={() => dispatch(incrementAsync(Number(inputValue) || 0))}
+        >
+          Increment Async
+        </button>
       </div>
     </div>
   )
